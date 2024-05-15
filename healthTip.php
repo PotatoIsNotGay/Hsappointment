fe6d423a797adaf782783b2044518fd5036f3882
Potato <luuarr344@gmail.com>
2024-05-14 20:30:16 -0700

    I add auth for blog updata and delete

--- healthTip.php
+++ healthTip.php
@@ -24,7 +24,6 @@ try {
 }
 ?>
 
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -64,55 +63,60 @@ try {
     </style>
 </head>
 <body style="overflow-x:hidden;">
-
-<main role="main" class="col-md-10 px-4 mx-auto">
-    <div class="pt-3 pb-2 mb-3 text-center">
-        <h1 class="h2 text-success">Health Tips</h1>
-        <hr class='border border-success border-3 opacity-75 '>
-    </div>
+    <?php include "nav.php"; ?>
     
-    <!-- Session status message display -->
-    <?php
-    if(isset($_SESSION['status'])) {
-        echo $_SESSION['status'];
-        unset($_SESSION['status']); // Clear the session status message
-    }
-    ?>
-    
-    <div class="row">
-        <?php foreach ($blogs as $blog) : ?>
-            <div class="col-12 col-md-6 col-lg-4 col-sm-12 mb-4">
-                <div class="card w-100 h-100" data-aos="flip-left">
-                    <img src="Photos/<?= $blog['image'] ?>" class="card-img-top" style="height:200px; object-fit: cover; border-top-left-radius: 10px; border-top-right-radius: 10px;" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title"><?= $blog['title'] ?></h5>
-                        <p class="card-text mt-4">
-                            <?php
-                            $string =  $blog['content'];
-                            $trimstring = substr($string, 0, 180);
-                            echo $trimstring;
-                            ?>
-                            <br>
-                            <br>
-                            <br>
-                            <a href="blog.php?id=<?= $blog['id'] ?>" class="btn btn-success float-start">See More</a>
-                            <!-- Update and Delete buttons -->
-                            <a href="views/updateBlog.php?id=<?= $blog['id'] ?>" class="btn btn-primary float-start mx-2">Update</a>
-                            <a href="actions/deleteBlog.php?id=<?= $blog['id'] ?>" class="btn btn-danger float-start">Delete</a>
-                        </p>
+    <main role="main" class="col-md-10 px-4 mx-auto">
+        <div class="pt-3 pb-2 mb-3 text-center">
+            <h1 class="h2 text-success">Health Tips</h1>
+            <hr class='border border-success border-3 opacity-75 '>
+        </div>
+        
+        <!-- Session status message display -->
+        <?php
+        if(isset($_SESSION['status'])) {
+            echo $_SESSION['status'];
+            unset($_SESSION['status']); // Clear the session status message
+        }
+        ?>
+        
+        <div class="row">
+            <?php foreach ($blogs as $blog) : ?>
+                <div class="col-12 col-md-6 col-lg-4 col-sm-12 mb-4">
+                    <div class="card w-100 h-100" data-aos="flip-left">
+                        <img src="Photos/<?= $blog['image'] ?>" class="card-img-top" style="height:200px; object-fit: cover; border-top-left-radius: 10px; border-top-right-radius: 10px;" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title"><?= $blog['title'] ?></h5>
+                            <p class="card-text mt-4">
+                                <?php
+                                $string =  $blog['content'];
+                                $trimstring = substr($string, 0, 180);
+                                echo $trimstring;
+                                ?>
+                                <br>
+                                <br>
+                                <br>
+                                <a href="blog.php?id=<?= $blog['id'] ?>" class="btn btn-success float-start">See More</a>
+                                <?php if(isset($_SESSION['admin_name'])): ?>
+                                    <!-- Update and Delete buttons -->
+                                    <a href="views/updateBlog.php?id=<?= $blog['id'] ?>" class="btn btn-primary float-start mx-2">Update</a>
+                                    <a href="actions/deleteBlog.php?id=<?= $blog['id'] ?>" class="btn btn-danger float-start">Delete</a>
+                                <?php endif; ?>
+                            </p>
+                        </div>
                     </div>
                 </div>
-            </div>
-        <?php endforeach ?>
-    </div>
-</main>
+            <?php endforeach ?>
+        </div>
+    </main>
 
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-<script>
-    AOS.init();
-</script>
+    <?php include "footer.php"; ?>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script>
+        AOS.init();
+    </script>
 </body>
 </html>
