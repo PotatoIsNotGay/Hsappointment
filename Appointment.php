5f1d7b7d2acbdc3ca6e0b5525ce81408a1729e61
Potato <luuarr344@gmail.com>
2024-05-27 11:46:26 -0700

    This is the final code of my project

--- Appointment.php
+++ Appointment.php
@@ -2,6 +2,7 @@
 session_start();
 include 'dbconn.php';
 
+
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
