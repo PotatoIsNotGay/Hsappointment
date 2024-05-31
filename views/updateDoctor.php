01c5c6eef80724ce7bd3d84545754b0fb572923f
Potato <luuarr344@gmail.com>
2024-05-30 21:44:35 -0700

    i updated updateDoctor.php

--- views/updateDoctor.php
+++ views/updateDoctor.php
@@ -50,70 +50,78 @@ if(isset($_SESSION['admin_name'])){
             <div class="card-header text-center h3 py-3 text-light">Update Doctor</div>
             <div class="card-body">
                 <form action="../actions/updateDoc.php" method="post" enctype="multipart/form-data">
-                    <label for="doctor_id" class="form-label text-light">Select Doctor:</label>
                     <?php
-                    // Fetch doctor's name based on ID
+                    // Fetch doctor's information based on ID
                     $doctor_id = $_GET['id'];
-                    $sql = "SELECT id, name FROM doctors WHERE id = :id";
+                    $sql = "SELECT * FROM doctors WHERE id = :id";
                     $stmt = $conn->prepare($sql);
                     $stmt->bindParam(':id', $doctor_id);
                     $stmt->execute();
                     $doctor = $stmt->fetch(PDO::FETCH_ASSOC);
+                    
+                    // Fetch current specialist and schedule IDs
+                    $current_specialist_id = $doctor['specialist_id'];
+                    $current_schedule1_id = $doctor['schedule1_id'];
+                    $current_schedule2_id = $doctor['schedule2_id'];
                     ?>
-                    <input type="hidden" name="doctor_id" id="doctor_id" value="<?php echo $doctor_id; ?>">
-                    <input type="text" class="form-control" value="<?php echo $doctor['name']; ?>" disabled>
-                    <br>
+                    
+                    <input type="hidden" name="doctor_id" value="<?php echo $doctor_id; ?>">
+                    
                     <label for="image" class="form-label text-light">New Image:</label>
                     <input type="file" name="image" id="image" class="form-control">
                     <br>
-                    <label for="name" class="form-label text-light">New Name:</label>
-                    <input type="text" name="name" id="name" class="form-control">
+                    
+                    <label for="name" class="form-label text-light">Name:</label>
+                    <input type="text" name="name" id="name" class="form-control" value="<?php echo htmlspecialchars($doctor['name']); ?>">
                     <br>
-                    <label for="age" class="form-label text-light">New Age:</label>
-                    <input type="text" name="age" id="age" class="form-control">
+                    
+                    <label for="age" class="form-label text-light">Age:</label>
+                    <input type="text" name="age" id="age" class="form-control" value="<?php echo htmlspecialchars($doctor['age']); ?>">
                     <br>
-                    <label for="qualification" class="form-label text-light">New Qualification:</label>
-                    <input type="text" name="qualification" id="qualification" class="form-control">
+                    
+                    <label for="qualification" class="form-label text-light">Qualification:</label>
+                    <input type="text" name="qualification" id="qualification" class="form-control" value="<?php echo htmlspecialchars($doctor['qualification']); ?>">
                     <br>
-                    <label for="specialists" class="form-label text-light">New Specialist Type:</label>
-                    <select class="form-select form-select-striped-md" name="specialists" id="specialists">
+                    
+                    <label for="specialists" class="form-label text-light">Specialist Type:</label>
+                    <select class="form-select" name="specialists" id="specialists">
                         <?php
                         $sql1 = "SELECT * FROM specialists";
                         $result1 = $conn->query($sql1);
-                        if(isset($result1)){
-                            while($row1 = $result1->fetch()){
-                                echo "<option value='".$row1['id']."'>".$row1['name']."</option>";
-                            }
+                        while($row1 = $result1->fetch()){
+                            $selected = ($row1['id'] == $current_specialist_id) ? 'selected' : '';
+                            echo "<option value='".$row1['id']."' $selected>".$row1['name']."</option>";
                         }
                         ?>
                     </select>
                     <br>
-                    <label for="sch1" class="form-label text-light">New Schedule Time 1:</label>
-                    <select class="form-select form-select-striped-md" name="sch1" id="sch1">
+                    
+                    <label for="sch1" class="form-label text-light">Schedule Time 1:</label>
+                    <select class="form-select" name="sch1" id="sch1">
                         <?php
                         $sql2 = "SELECT * FROM schedules1";
                         $result2 = $conn->query($sql2);
-                        if(isset($result2)){
-                            while($row2 = $result2->fetch()){
-                                echo "<option value='".$row2['id']."'>".$row2['time']."</option>";
-                            }
+                        while($row2 = $result2->fetch()){
+                            $selected = ($row2['id'] == $current_schedule1_id) ? 'selected' : '';
+                            echo "<option value='".$row2['id']."' $selected>".$row2['time']."</option>";
                         }
                         ?>
                     </select>
                     <br>
-                    <label for="sch2" class="form-label text-light">New Schedule Time 2:</label>
-                    <select class="form-select form-select-striped-md" name="sch2" id="sch2">
+                    
+                    <label for="sch2" class="form-label text-light">Schedule Time 2:</label>
+                    <select class="form-select" name="sch2" id="sch2">
                         <?php
                         $sql3 = "SELECT * FROM schedules2";
                         $result3 = $conn->query($sql3);
-                        if(isset($result3)){
-                            while($row3 = $result3->fetch()){
-                                echo "<option value='".$row3['id']."'>".$row3['time']."</option>";
-                            }
+                        while($row3 = $result3->fetch()){
+                            $selected = ($row3['id'] == $current_schedule2_id) ? 'selected' : '';
+                            echo "<option value='".$row3['id']."' $selected>".$row3['time']."</option>";
                         }
                         ?>
                     </select>
                     <br>
+                    
                     <input type="submit" class="btn form-control btn-dark" value="Update">
                 </form>
             </div>
